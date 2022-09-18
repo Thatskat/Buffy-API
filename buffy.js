834a8671adcc3929694c43cfe39da4efca8652b4
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-09-18 15:04:56 +1000

    completed js

--- buffy.js
+++ buffy.js
@@ -7,9 +7,8 @@ const seasonep = document.getElementById("season-ep");
 const plot = document.getElementById("plot");
 const airdate = document.getElementById("airdate");
 const director = document.getElementById("director");
-const writers = document.getElementById("writers");
 const quote = document.getElementById("quote");
-const eptriivia = document.getElementById("eptrivia");
+const eptrivia = document.getElementById("eptrivia");
 const imdblink = document.getElementById("imdblink");
 
 const episodeinformation = document.getElementById("episode-information");
@@ -18,8 +17,6 @@ const errorMessage = document.getElementById("error");
 const apiURL = 'https://buffy-the-vampire-slayer-api.herokuapp.com/';
 const apiEpisode = 'episode/';
 const apiSeason = '/season/';
-const IMDB = 'https://www.imdb.com/title/tt0118276/?';
-
 
 episodeinformation.style.display = "none";
 errorMessage.style.display = "none";
@@ -43,4 +40,23 @@ async function findData(seasonQuery, episodeQuery) {
   } catch (error) {
     errorMessage.style.display = "block";
   }
+}
+
+function displayEpisode (data) {
+  console.log(data.Error);
+  if(data.Error) {
+    errorMessage.style.display = "block";
+    episodeinformation.style.display = "none";
+    errorMessage.innerHTML = "This Episode of BTVS could not be found 😒 Try Again!";
+    return;
+  }
+  episodeinformation.style.display="block";
+  eptitle.innerHTML = data[0].title;
+  seasonep.innerHTML = "Season " + data[0].season + " Episode " + data[0].episode_number;
+  plot.innerHTML = '<i>"' + data[0].plot + '"</i>';
+  airdate.innerHTML = "Original Broadcast Date: " + data[0].air_date;
+  director.innerHTML = "Directed by " + data[0].director;
+  quote.innerHTML = data[0].quote;
+  eptrivia.innerHTML = "Episode Trivia: " + data[0].trivia;
+  imdblink.href = data[0].imdb_url;
 }
