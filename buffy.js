414e456cc59f4403e148fe7f33a5f9e6c2187d5b
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-09-18 14:19:39 +1000

    added getelements by ids

---
+++ buffy.js
@@ -0,0 +1,6 @@
+// Get variables from document
+const season = document.getElementById("season");
+const episode = document.getElementById("epnumber");
+const btn = document.getElementById("submitbtn");
+
+// Api 
